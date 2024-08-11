95d456f2465c431b73a6ebb3ae8adbaa35b80565
Prabhat Kumar Shukla <prabhat.shukla.1998@gmail.com>
2024-08-11 10:23:06 +0530

    set application properties, generated dynamic html using thymeleaf -- page controller and home.html

---
+++ src/main/java/com/scm/scm2_0/Controllers/PageController.java
@@ -0,0 +1,22 @@
+package com.scm.scm2_0.Controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class PageController {
+
+    @RequestMapping("/home")
+    public String home(Model model){
+        
+        System.out.println("Home Page Handler");
+
+        // Sending data to view
+        model.addAttribute("name", "Substring Technologies");
+        model.addAttribute("YouTubeChannel", "LCWD");
+        model.addAttribute("githubLink", "https://github.com/pshukla5/Group1--Project");
+        return "Home";
+    }
+
+}
---
+++ src/test/java/com/scm/scm2_0/SmartContactManagerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.scm.scm2_0;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SmartContactManagerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
